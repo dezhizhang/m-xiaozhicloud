111fcea30584cca8642eb5ec5e8f2dd60a5b9db3
dezhizhang <1541609448@qq.com>
2023-06-19 22:20:53 +0800

    feat:添加新闻类型

--- app/model/intelligent.js
+++ app/model/intelligent.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Xiaozhi
  * :date created: 2022-11-19 09:01:10
  * :last editor: 张德志
- * :date last edited: 2023-06-19 19:42:44
+ * :date last edited: 2023-06-19 22:19:14
  */
 'use strict';
 module.exports = app => {
@@ -18,10 +18,10 @@ module.exports = app => {
     url: { type: String }, // 图片链接
     status: { type: String }, // 排序
     type: { type: String }, // 类型
-    position: { type: String },
+    description: { type: String },
     topClassify: { type: String }, // 一级分类
     secondaryClassify: { type: String }, // 二级分类
-    updateTime: {
+    update_time: {
       type: Number,
     },
     add_time: {
