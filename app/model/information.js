bdcc7350bd155a6532dda284708f11775ea0fbc5
dezhizhang <1541609448@qq.com>
2023-06-28 13:50:26 +0800

    feat:更新

--- app/model/information.js
+++ app/model/information.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Xiaozhi
  * :date created: 2022-11-19 09:01:10
  * :last editor: 张德志
- * :date last edited: 2023-06-19 22:20:04
+ * :date last edited: 2023-06-27 23:49:18
  */
 'use strict';
 module.exports = app => {
