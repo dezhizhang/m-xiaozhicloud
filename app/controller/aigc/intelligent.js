e89706d23aaef9ccd259cd3602b6660231dcb299
dezhizhang <1541609448@qq.com>
2023-10-02 12:02:48 +0800

    feat:更新条件

--- app/controller/aigc/intelligent.js
+++ app/controller/aigc/intelligent.js
@@ -6,7 +6,7 @@
  * :copyright: (c) 2022, Xiaozhi
  * :date created: 2022-11-19 09:00:04
  * :last editor: 张德志
- * :date last edited: 2023-06-25 16:38:55
+ * :date last edited: 2023-10-02 12:02:22
  */
 'use strict';
 
@@ -18,21 +18,21 @@ class IntelligentController extends Controller {
     const ctx = this.ctx;
     const body = ctx.request.body;
     await this.service.intelligent.create(body);
-    ctx.helper.success({ ctx, res: '新增人工智能产品成功' });
+    await ctx.helper.success(ctx, '新增人工智能产品成功');
   }
 
   async edit() {
     const ctx = this.ctx;
     const body = ctx.request.body;
     await this.service.intelligent.update(body._id, body);
-    ctx.helper.success({ ctx, res: [], msg: '编辑人工智能产品成功' });
+    await ctx.helper.success(ctx, '编辑人工智能产品成功');
   }
 
   async info() {
     const ctx = this.ctx;
     const query = ctx.request.query;
     const res = await this.service.intelligent.info(query?.id);
-    ctx.helper.success({ ctx, res, mes: '获取数据成功' });
+    await ctx.helper.success(ctx, '获取数据成功', res);
   }
 
   async list() {
@@ -42,7 +42,7 @@ class IntelligentController extends Controller {
     // 调用 Service 进行业务处理
     const res = await service.intelligent.list(body);
     // // 设置响应内容和响应状态码
-    ctx.helper.success({ ctx, res });
+    await ctx.helper.success(ctx, '获取数据成功', res);
   }
 
   // 删除网站
@@ -50,7 +50,7 @@ class IntelligentController extends Controller {
     const ctx = this.ctx;
     const body = ctx.request.body;
     await this.service.intelligent.destroy(body._id);
-    ctx.helper.success({ ctx, res: '删除人工智能产品成功' });
+    await ctx.helper.success(ctx, '删除人工智能产品成功');
   }
 }
 
